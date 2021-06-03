4225b61dcc553351e949485362cda7a6422c909f
kanakan12 <69573636+kanakan12@users.noreply.github.com>
2021-06-03 15:48:14 +0900

    Added Movie URL

--- graphql/db.js
+++ graphql/db.js
@@ -1,59 +1,14 @@
-let movies = [
-    {
-        id: "0",
-        name: "A",
-        score: 5,
-    },
-    {
-        id: "1",
-        name: "B",
-        score: 4,
-    },
-    {
-        id: "2",
-        name: "C",
-        score: 3,
-    },
-    {
-        id: "3",
-        name: "D",
-        score: 2,
-    },
-    {
-        id: "4",
-        name: "E",
-        score: 1
-    },
-    {
-        id: "5",
-        name: "F",
-        score: 0
+import fetch from "node-fetch";
+
+const API_URL = "https://yts.mx/api/v2/list_movies.json?"
+
+export const getMovies = (limit, rating) => {
+    let REQUEST_URL = API_URL;
+    if(limit > 0) {
+        REQUEST_URL += `limit=${limit}`;
     }
-]
-
-export const getMovies = () => movies;
-
-export const getById = id => {
-    const filteredMovies = movies.filter(movie => movie.id === String(id));
-    return filteredMovies[0];
-}
-
-export const deleteMovie = (id) => {
-    const cleanedMovies = movies.filter(movie => movie.id !== String(id));
-    if(movies.length > cleanedMovies.length) {
-        movies = cleanedMovies;
-        return true;
-    } else {
-        return false;
+    if(rating > 0) {
+        REQUEST_URL += `&minimum_rating=${rating}`;
     }
-}
-
-export const addMovie = (name, score) => {
-    const newMovie = {
-        id: `${movies.length + 1}`,
-        name,
-        score
-    };
-    movies.push(newMovie);
-    return newMovie;
+    return fetch(REQUEST_URL).then(res => res.json()).then(json => json.data.movies);
 }
